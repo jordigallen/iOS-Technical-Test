f9c158c60263478e81023549011bffe1f1b89863
Jordi Gallen <jordigr.bcn@gmail.com>
2021-03-24 13:29:10 +0100

    Upload coverage test

--- ios-Technical-TestTests/Source/Core/Remote/RemoteConfigurationTests.swift
+++ ios-Technical-TestTests/Source/Core/Remote/RemoteConfigurationTests.swift
@@ -10,9 +10,51 @@ import XCTest
 @testable import ios_Technical_Test
 
 final class RemoteConfigurationTests: XCTestCase {
+    let remoteManager = RemoteManager(RemoteConfiguration(baseUrl: "test"))
+
     func testRemoteConfigurationInstanceConstructor() {
         let remoteConfiguration = RemoteConfiguration(baseUrl: "URL_TEST")
         expect(remoteConfiguration).notTo(beNil())
+    }
+
+    func testRemoteGet(){
+        self.remoteManager.get("test", parameters: nil) { (result: Swift.Result<[TVShowModel], BaseError>) in
+            switch result {
+            case let .success(value):
+                expect(value).notTo(beNil())
+            case .failure(_): break
+            }
+        }
+    }
+
+    func testRemotePost(){
+        self.remoteManager.post("test", parameters: nil) { (result: Swift.Result<[TVShowModel], BaseError>) in
+            switch result {
+            case let .success(value):
+                expect(value).notTo(beNil())
+            case .failure(_): break
+            }
+        }
+    }
+
+    func testRemoteDelete(){
+        self.remoteManager.delete("test", parameters: nil) { (result: Swift.Result<[TVShowModel], BaseError>) in
+            switch result {
+            case let .success(value):
+                expect(value).notTo(beNil())
+            case .failure(_): break
+            }
+        }
+    }
+
+    func testRemotePut(){
+        self.remoteManager.put("test", parameters: nil) { (result: Swift.Result<[TVShowModel], BaseError>) in
+            switch result {
+            case let .success(value):
+                expect(value).notTo(beNil())
+            case .failure(_): break
+            }
+        }
     }
 }
  
